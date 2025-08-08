8ef98306a011db47f2e80bce6e685712e265ef56
kindtiger <seonbeom2@gmail.com>
2025-08-08 15:51:46 +0900

    learn. wather 설계와 이유

--- vue-practice/src/components/Chapter.vue
+++ vue-practice/src/components/Chapter.vue
@@ -1,101 +1,13 @@
-<!-- 
-
-vue의 리스트 렌더링은 v-for 디렉티브를 사용하여 만들 수 있습니다.
-vue는 내부적으로 proxy를 사용하기 때문에 immutable 데이터를 강제하지 않습니다.
-
-
--->
-
 <script setup lang="ts">
-import { ref } from 'vue';
-
-
-let id = 0;
-
-interface Todo {
-  id: number;
-  text: string;
-  done: boolean;
-}
-
-const todos = ref<Todo[]>([
-  {id:id++, text:'HTML 배우기', done:false},
-  {id:id++, text:'CSS 배우기', done:false},
-  {id:id++, text:'JavaScript 배우기', done:false},
-  {id:id++, text:'Vue 배우기', done:false},
-])
-
-const newTodo = ref('');
-const hideCompleted = ref(false);
-
-
-const removeTodo = (id:number) => {
-  todos.value = todos.value.filter(todo => todo.id !== id);
-}
-
-const addTodo = () => {
-  if(newTodo.value.trim()){
-    todos.value.push({
-      id: id++,
-      text: newTodo.value.trim(),
-      done: false
-    })
-    newTodo.value = '' // 입력 필드 초기화
-  }
-}
-
-
-const filteredTodo = () => {
-  return hideCompleted.value ? todos.value.filter(t => !t.done) : todos.value
-}
-
 
 </script>
 
-
 <template>
+  <div>
 
-  <form @submit.prevent="addTodo">
-    <input type="text" v-model="newTodo">
-    <button type="submit">할 일 추가</button>
-  </form>
-
-  <div v-if="todos.length > 0">총 할 일 : {{ filteredTodo().length }} 개</div>
-  <div v-else>할 일 끝!</div>
-  <ul>
-    <li v-for="todo in filteredTodo()" :key="todo.id">
-      <input type="checkbox" :id="`${todo.id}`" v-model="todo.done">
-      <label :for="`${todo.id}`">
-        <span :class="{done:todo.done, text:'newValue'}">{{ todo.text }}</span>
-      </label>
-      <button type="button" @click="()=> removeTodo(todo.id)">❌</button>
-    </li>
-  </ul>
-
-  <button type="button" @click="hideCompleted = !hideCompleted">
-    {{ hideCompleted ? 'show all' : 'hide completed' }}
-  </button>
-
+  </div>
 </template>
 
 <style scoped>
-
-.done{
-  text-decoration: line-through;
-}
-
-form{
-  margin-bottom: 1rem;
-}
-
-ul{
-  li{
-    margin-bottom: 1rem;
-  }
-}
-
-button{
-  margin-left: 2rem;
-}
 
 </style>
