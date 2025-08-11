5d45e3c444af2a20bfb116d3a0c53a475b6e88f2
kindtiger <seonbeom2@gmail.com>
2025-08-11 09:50:23 +0900

    learn. emit 으로 이벤트 전달하기

---
+++ vue-practice/src/components/ChildComp.vue
@@ -0,0 +1,31 @@
+
+
+<script setup lang="ts">
+
+
+const props = defineProps<{
+  score: number,
+  comment: string
+}>()
+
+
+</script>
+
+<template>
+  <div>
+    <input 
+      type="range" 
+      min="0" 
+      max="5"
+    >
+    
+    <textarea
+
+    ></textarea>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
